ae250c504d71f77f5cf7a1fa8c4fe5d1ba2b9473
Jonas Schmid <jonas.schmid@fairtiq.com>
2024-07-10 17:07:18 +0200

    Introduce conflicting SDKs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://datatrans.jfrog.io/artifactory/mobile-sdk/") }
     }
 }
 
